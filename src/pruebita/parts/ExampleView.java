f7ce78e74c59dc1de7475cf797b8e9d9b5c78369
Karina Martinez Guerrero <karinamguerrero98@gmail.com>
2017-10-19 17:01:52 -0600

    Problemas para cargar con imagenes el scrolledComposite

--- src/pruebita/parts/ExampleView.java
+++ src/pruebita/parts/ExampleView.java
@@ -1,21 +1,61 @@
 package pruebita.parts;
 
-import javax.inject.Inject;
+import java.util.ArrayList;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
 
 public class ExampleView {
 
-	//@Inject
-	//ESelectionService selectionService;
-	
-	@Inject
-	public ExampleView(Composite parent) {
-		Label label = new Label(parent, SWT.NONE);
-		label.setText("Hello World! Here I am");
-	}
+	public static void main(String[] args) {
 
+        // setup the SWT window
+        Display display = new Display();
+        final Shell shell = new Shell(display);
+        FillLayout fillLayout = new FillLayout(SWT.VERTICAL);
+        shell.setLayout(fillLayout);
+        shell.setText("Photo Application");
 
+        // initialize a parent composite with a grid layout manager
+        // with 5x columns
+        Composite parent = new Composite(shell, SWT.NONE);
+        parent.setLayout(fillLayout);
+
+        // Get the Display default icons
+        ArrayList<Image> imageList = new ArrayList<Image>();
+
+        imageList.add(Display.getDefault().getSystemImage(SWT.ICON_WARNING));
+        imageList.add(Display.getDefault().getSystemImage(SWT.ICON_WORKING));
+        imageList.add(Display.getDefault().getSystemImage(SWT.ICON_QUESTION));
+        imageList.add(Display.getDefault().getSystemImage(SWT.ICON_INFORMATION));
+        imageList.add(Display.getDefault().getSystemImage(SWT.ICON_ERROR));
+
+        for (Image image : imageList) {
+            Label label = new Label(parent, SWT.NONE);
+            label.setImage(image);
+        }
+        // show the SWT window
+
+        shell.pack();
+        shell.open();
+
+        while (!shell.isDisposed()) {
+            if (!display.readAndDispatch())
+                display.sleep();
+        }
+        // tear down the SWT window
+        display.dispose();
+        // if you do not use system images you would have to release them
+        // not necessary in this example
+        // for (Image image : imageList) {
+        //    if (image != null) {
+        //     image.dispose();
+        //    }
+        //    }
+    }
 }
